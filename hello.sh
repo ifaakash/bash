f225e0e0fe3aa0ba7d40a369da2aeaa500c808a9
ifaakash <aakashc.appregis@gmail.com>
2026-03-01 21:54:12 +0530

    feat: add cron job, script, and initial logs for automated task execution
    - Add `cron-sample.txt` with cron configuration to run script every 2 minutes
    - Add `hello.sh` bash script that displays system info and exit codes
    - Include initial execution logs in `tmp/logs.txt`

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+os="$(uname -a)"
+
+echo "#-----------------------------------------"
+echo "Hello, my friend! I am a Linux Moo"
+
+exit_code=$?
+echo "The exit code for above command is ${exit_code}"
+echo "#-----------------------------------------"
