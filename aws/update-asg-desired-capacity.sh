86a8e5d3556aad99c006ff0afc30384261411c3a
ifaakash <aakashc.appregis@gmail.com>
2026-07-11 12:54:30 +0530

    chore: update echo to printf

--- aws/update-asg-desired-capacity.sh
+++ aws/update-asg-desired-capacity.sh
@@ -3,4 +3,13 @@
 printf 'Enter AWS Region\n'
 read region
 
- 
+printf 'Desired capacity\n'
+read desired_count
+
+printf 'Enter ASG name\n'
+read asg_name
+
+printf "–––––––––– Updating ASG desired capacity –––––––––––\n"
+aws autoscaling update-auto-scaling-group \
+  --auto-scaling-group-name $asg_name \
+  --desired-capacity $desired_count
