75f52209089a3e2688d74de697fd0ae3447c923e
ifaakash <aakashc.appregis@gmail.com>
2026-07-12 13:04:15 +0530

    chore: Add conditional statement to update asg count or not?

--- aws/update-asg-desired-capacity.sh
+++ aws/update-asg-desired-capacity.sh
@@ -5,19 +5,28 @@ aws autoscaling describe-auto-scaling-groups \
   --query 'AutoScalingGroups[].AutoScalingGroupName' \
   --output table
 
-printf 'Enter AWS Region\n'
-read region
+printf "Want to update the ASG desired count?"
+while true; do
+    read -p "Enter Y to continue: " choice
+    if [ "$choice" == 'Y' ]; then
+		printf 'Enter AWS Region\n'
+		read region
 
-printf 'Desired capacity\n'
-read desired_count
+		printf 'Desired capacity\n'
+		read desired_count
 
-printf 'Enter ASG name\n'
-read asg_name
+		printf 'Enter ASG name\n'
+		read asg_name
 
-printf "–––––––––– Updating ASG desired capacity –––––––––––\n"
-aws autoscaling update-auto-scaling-group \
-  --auto-scaling-group-name $asg_name \
-  --desired-capacity $desired_count
+		printf "–––––––––– Updating ASG desired capacity –––––––––––\n"
+		aws autoscaling update-auto-scaling-group \
+		  --auto-scaling-group-name $asg_name \
+		  --desired-capacity $desired_count
+     fi
+     else
+       break
+     fi
+done
 
 
 # aws autoscaling update-auto-scaling-group \
