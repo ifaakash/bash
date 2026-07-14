ee388be6dd0f64045d59d747c3dde636d3a653b3
ifaakash <aakashc.appregis@gmail.com>
2026-07-14 14:56:22 +0530

    fix: add validation for empty key selection in ssh-key-add.sh

--- ssh-key-add.sh
+++ ssh-key-add.sh
@@ -7,9 +7,14 @@ list=$(ls $SSH_DIRECTORY)
 
 printf "Choose the ssh key to add:\n"
 key_name=$(gum choose $list)
- 
-printf "Initialising new ssh agent\n"
-eval "ssh-agent -s"
- 
-printf "Adding ssh key $key_name"
-ssh-add $SSH_DIRECTORY/$key_name
+
+if [ $key_name == '']; then
+  printf "––––––––– EXITING - no key selected –––––––––"
+  break
+else
+	printf "Initialising new ssh agent\n"
+	eval "ssh-agent -s"
+	 
+	printf "Adding ssh key $key_name"
+	ssh-add $SSH_DIRECTORY/$key_name
+fi
