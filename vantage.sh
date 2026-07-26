995adc4ee70f4c20947f75bc1021b1210c85fada
ifaakash <aakashc.appregis@gmail.com>
2026-07-27 02:24:25 +0530

    feat: add initial EC2 instance class selection script and .gitignore
    - Add .gitignore to exclude JSON files
    - Create vantage.sh with basic EC2 instance class selection using T3 family

---
+++ vantage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+printf "Choose T/M for balanced workloads, C for CPU-heavy work, R/X for memory-heavy work, I/D/H for storage-heavy work, and P/G/F when you need hardware acceleration\n"
+
+printf "Select the instance class ( only t3 family is supported ):\n"
+# T3 family
+alist= ["t3.nano", "t3.micro", "t3.small", "t3.medium", "t3.large", "t3.xlarge", "t3.2xlarge"]
+aInstanceClass=$(gum choose alist)
+
+# get instance class
+agetInstanceEndpoint="/api/v1/instances/ec2/"
+# use gum to scroll over the instance class
+
+printf "Curling the endpoint for /api/v1/instances/{service}/{instanceType}/global\n"
+
+if /api/v1/instances/{service}/{instanceType}/global
