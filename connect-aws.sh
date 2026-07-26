c1693671a1dd0964fdeda89779bcaa883ea82f12
ifaakash <aakashc.appregis@gmail.com>
2026-07-26 23:01:16 +0530

    feat(connect-aws): add profile session check before SSO login

--- connect-aws.sh
+++ connect-aws.sh
@@ -7,9 +7,15 @@ aprofiles=$(grep '^\[.*\]$' "$SSM_HOME_DIRECTORY" | tr -d '[]' | sed 's/^profile
 aselected_profile=$(gum choose $aprofiles)
 
 printf "–––––––––- Connecting to AWS profile  –––––––––––\n"
-aws sso login --profile $aselected_profiles
+
+if aws sts get-caller-identity --profile $aselected_profile  > /dev/null 2>&1; then
+   printf "Already logged into the profile ${aselected_profiles}\n Skipping the login step!"
+else
+   printf "Needs authentication. Opening browser..."
+   if  ! aws sso login --profile $aselected_profile; then
+       printf "Skipping login due to failure"
+       continue
+   fi
+fi
 
 printf "–––––––––-  –––––––––––\n"
-
-
-
