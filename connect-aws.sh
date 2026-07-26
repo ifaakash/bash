9908d3c89d5bfa7e30611f15a1ce5343d4b79990
ifaakash <aakashc.appregis@gmail.com>
2026-07-26 22:44:37 +0530

    fix: quote variable and strip "profile " prefix from AWS profile names

--- connect-aws.sh
+++ connect-aws.sh
@@ -2,7 +2,7 @@
 
 SSM_HOME_DIRECTORY="$HOME/.aws/config"
 printf "–––––––––- List AWS profiles in SSM  –––––––––––\n"
-profiles=$(grep '^\[.*\]$' $SSM_HOME_DIRECTORY | tr -d '[]')
+profiles=$(grep '^\[.*\]$' "$SSM_HOME_DIRECTORY" | tr -d '[]' | sed 's/^profile //')
 
 printf "Listing AWS Profiles:\n" $profiles
 
