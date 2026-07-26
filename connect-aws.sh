2624bd6195a142330736e752035805716b5339ec
ifaakash <aakashc.appregis@gmail.com>
2026-07-26 22:49:04 +0530

    fix: correct variable name in gum choose command
    The variable `$profiles` was incorrectly referencing an undefined variable. Changed to `$aprofiles` to use the actual list of AWS profiles from SSM configuration.

--- connect-aws.sh
+++ connect-aws.sh
@@ -4,7 +4,7 @@ SSM_HOME_DIRECTORY="$HOME/.aws/config"
 printf "–––––––––- List AWS profiles in SSM  –––––––––––\n"
 aprofiles=$(grep '^\[.*\]$' "$SSM_HOME_DIRECTORY" | tr -d '[]' | sed 's/^profile //')
 
-aselected_profile=$(gum choose $profiles)
+aselected_profile=$(gum choose $aprofiles)
 
 printf "–––––––––- Connecting to AWS profile  –––––––––––\n"
 aws sso login --profile $aselected_profiles
