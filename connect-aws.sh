adb58bb9f03f2cda1f82b02e311663c580b19f89
ifaakash <aakashc.appregis@gmail.com>
2026-07-26 22:48:23 +0530

    fix: use gum to select and log in to AWS SSO profile

--- connect-aws.sh
+++ connect-aws.sh
@@ -2,9 +2,12 @@
 
 SSM_HOME_DIRECTORY="$HOME/.aws/config"
 printf "–––––––––- List AWS profiles in SSM  –––––––––––\n"
-profiles=$(grep '^\[.*\]$' "$SSM_HOME_DIRECTORY" | tr -d '[]' | sed 's/^profile //')
+aprofiles=$(grep '^\[.*\]$' "$SSM_HOME_DIRECTORY" | tr -d '[]' | sed 's/^profile //')
 
-printf "Listing AWS Profiles:\n" $profiles
+aselected_profile=$(gum choose $profiles)
+
+printf "–––––––––- Connecting to AWS profile  –––––––––––\n"
+aws sso login --profile $aselected_profiles
 
 printf "–––––––––-  –––––––––––\n"
 
