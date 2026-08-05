41562023cc18ebd1e44dce394cade0aabc344ee5
ifaakash <aakashc.appregis@gmail.com>
2026-08-05 14:40:14 +0530

    feat: Add list instance script

---
+++ list-ec2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+printf "–––––––––- LISTING EC2 Instances  –––––––––––\n"
+availableRegion=("us-east-1" "us-east-2")
+region=$(gum choose ${availableRegion[@]})
+printf "Setting $region as default region for all AWS commands for this session\n"
+
+# export "AWS_REGION"=$region
+# aws ec2 describe-instances --region ap-south-1
+
