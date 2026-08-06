bf044854bc2f9b52e78007def79dd3404bc6a10d
ifaakash <aakashc.appregis@gmail.com>
2026-08-06 23:41:34 +0530

    fix: correct ENI info parsing and output in curl result script

--- vantage.sh
+++ vantage.sh
@@ -16,13 +16,14 @@ curlResult=$(curl -s -f "$targetUrl")
 if [ $? -eq 0 ]; then
    printf "Curl passed!\n"
    instanceType=$(echo "$curlResult" | jq -r '.instance_type')
+#   echo $curlResult > curlResult.json
    vCPU=$(echo "$curlResult" | jq -r '.vCPU')
    memory=$(echo "$curlResult" | jq -r '.memory')
-   enis=$(echo "$curlResult" | jq -r '.vpc')
-   echo $instanceType
-   echo $vCPU
-   echo $memory
-   echo $enis
+   enis=$(echo "$curlResult" | jq -r '.vpc.max_enis')
+   printf "Instance type $instanceType has $vCPU vCPU and $memory GB memory; contains $enis eni\'s"
+   #echo $vCPU
+   #echo $memory
+   #echo $enis
 else
    printf "Curl failed!"
 fi
