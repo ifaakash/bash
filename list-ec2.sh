3012374546ac35e86009d37f6c949fd2bd4706d5
ifaakash <aakashc.appregis@gmail.com>
2026-08-07 15:21:46 +0530

    fix: use source for env variables and add instance selection to EC2 list script

--- list-ec2.sh
+++ list-ec2.sh
@@ -3,10 +3,16 @@
 printf "–––––––––- LISTING EC2 Instances  –––––––––––\n"
 availableRegion=("us-east-1" "us-east-2")
 printf "Setup the required environment variables..."
-./env.sh
+source .env.sh
 region=$(gum choose ${availableRegion[@]})
 printf "Setting $region as default region for all AWS commands for this session\n"
 
 # export "AWS_REGION"=$region
-# aws ec2 describe-instances --region ap-south-1
+selected_instance=$(aws ec2 describe-instances \
+    --query 'Reservations[].Instances[].[InstanceId, Tags[?Key==`Name`]|[0].Value]' \
+    --region "$region" --profile particle --output text \
+    | gum filter --header "Choose an EC2 instance:" \
+    | awk '{print $1}')
+
+printf "Selected instance: %s\n" "$selected_instance"
 
