7b37f7878ba8beb6fc26868e09565c8c77dfa8d4
ifaakash <aakashc.appregis@gmail.com>
2026-08-07 14:33:49 +0530

    feat(aws): comment out region export in connect script

--- connect-aws.sh
+++ connect-aws.sh
@@ -24,6 +24,7 @@ printf "–––––––––- LISTING EC2 Instances  ––––––�
 availableRegion=("us-east-1" "us-east-2")
 region=$(gum choose ${availableRegion[@]})
 printf "Setting $region as default region for all AWS commands for this session\n"
-export "AWS_REGION"=$region
+# export "AWS_REGION"=$region
+# aws ec2 describe-instances --region ap-south-1
 
 
