a29a6310215c55e5d376e15c4c3b926e064b2aea
ifaakash <aakashc.appregis@gmail.com>
2026-08-09 18:48:42 +0530

    refactor: remove the sleep with kill command and wrap the aws command in gum

--- list-ec2.sh
+++ list-ec2.sh
@@ -11,31 +11,38 @@ printf "Setting $region as default region for all AWS commands for this session\
 gum spin --spinner dot --title "Fetching instances in $region region" -- sleep 3
 selectedInstance=$(aws ec2 describe-instances \
     --query 'Reservations[].Instances[].[InstanceId, Tags[?Key==`Name`]|[0].Value]' \
-    --region "$region" --profile particle --output text \
+    --region "$region" --profile $profile --output text \
     | gum filter --header "Choose an EC2 instance:" \
     | awk '{print $1}')
 
 # printf "Selected instance: %s\n" "$selectedInstance"
 # printf "Starting SSM session to instance $selectedInstance\n"
-gum spin --spinner dot --title  "Checking the status of instance with ID $selectedInstance" -- sleep 15 &
-SPIN_PID=$!
+#gum spin --spinner dot --title  "Checking the status of instance with ID $selectedInstance" -- sleep 15 &
+#SPIN_PID=$!
 
 # echo $SPIN_PID
-instanceState=$(aws ec2 describe-instances --instance-ids \
-   "$selectedInstance" --query "Reservations[*].Instances[*].State.Name" \
-   --output text --profile particle)
 
-kill $SPIN_PID 2>/dev/null
-wait $SPIN_PID 2>/dev/null
+instanceState=$(gum spin --spinner dot --title "Checking the status of instance with ID $selectedInstance" \
+    -- aws ec2 describe-instances --instance-ids \
+   "$selectedInstance" --query "Reservations[*].Instances[*].State.Name" \
+   --output text --profile $profile )
+
+#kill $SPIN_PID 2>/dev/null
+#wait $SPIN_PID 2>/dev/null
 
 if [[ "$instanceState" == "stopped" ]]; then
-     printf "Instance is in stopped state! Do you want to start the instance?\n"
+     printf "Instance is in STOPPED state! Do you want to start the instance?\n"
      choice=$(gum choose "Yes" "No")
      if [[ "$choice" == "Yes" ]]; then
-        printf "Starting instance\n"
+        printf "STARTING instance\n"
         aws ec2 start-instances --instance-ids $selectedInstance \
-            --region $region --profile particle
+            --region $region --profile $profile
+        printf "Instance is RUNNING now!\n"
      else
         printf "Skipping start action\n"
      fi
 fi
+
+
+printf "STARTING SSM SESSION TO THE INSTANCE\n"
+aws ssm start-session --target $selectedInstance --profile $profile --region $region
