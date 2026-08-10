b83fc894e92966102a170c078620a0ef4c806058
ifaakash <aakashc.appregis@gmail.com>
2026-08-10 16:37:58 +0530

    fix: add loading indicators and quote instance ID in EC2 script
    - Add spinner for fetching instances in region and checking instance status
    - Enclose instance ID in quotes to handle potential special characters

--- list-ec2.sh
+++ list-ec2.sh
@@ -8,6 +8,7 @@ region=$(gum choose ${availableRegion[@]})
 printf "Setting $region as default region for all AWS commands for this session\n"
 
 # export "AWS_REGION"=$region
+gum spin --spinner dot --title "Fetching instances in $region region" -- sleep 3
 selectedInstance=$(aws ec2 describe-instances \
     --query 'Reservations[].Instances[].[InstanceId, Tags[?Key==`Name`]|[0].Value]' \
     --region "$region" --profile particle --output text \
@@ -16,8 +17,10 @@ selectedInstance=$(aws ec2 describe-instances \
 
 # printf "Selected instance: %s\n" "$selectedInstance"
 # printf "Starting SSM session to instance $selectedInstance\n"
+gum spin --spinner dot --title  "Checking the status of instance with ID $selectedInstance" -- sleep 5
+
 aws ec2 describe-instances --instance-ids \
-   $selectedInstance --query "Reservations[*].Instances[*].State.Name" \
+   "$selectedInstance" --query "Reservations[*].Instances[*].State.Name" \
    --output text --profile particle
 # Fetch state of instance
 # Provide option to on/off the instance - if on, then off and vice-versa
